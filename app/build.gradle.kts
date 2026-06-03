0d7d46f9a0826b33688bf77850d402005c9eba63
if <1096005725@qq.com>
2026-06-03 22:14:02 +0800

    v1.9.56: Separate number and unit alignment in InfoBlock

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.skypulse.weather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 235
-        versionName = "1.9.55"
+        versionCode = 236
+        versionName = "1.9.56"
 
         vectorDrawables {
             useSupportLibrary = true
