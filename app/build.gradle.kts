7e4b38f46052fdaa6140a8ab0713152eb7033889
if <1096005725@qq.com>
2026-06-03 22:17:54 +0800

    v1.9.57: Fix InfoBlock alignment with Box layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.skypulse.weather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 236
-        versionName = "1.9.56"
+        versionCode = 237
+        versionName = "1.9.57"
 
         vectorDrawables {
             useSupportLibrary = true
