8bc829047ecd6752126972b693c61719419e449b
if <1096005725@qq.com>
2026-07-06 00:04:02 +0800

    Simplify widget flat background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.skypulse.weather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 710
-        versionName = "3.1.37"
+        versionCode = 712
+        versionName = "3.1.39"
 
         vectorDrawables {
             useSupportLibrary = true
