7b9c3b5715477620434ec59c266cb043a106f0d9
if <1096005725@qq.com>
2026-07-06 11:52:04 +0800

    chore: bump version to 3.1.53

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.skypulse.weather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 725
-        versionName = "3.1.52"
+        versionCode = 726
+        versionName = "3.1.53"
 
         vectorDrawables {
             useSupportLibrary = true
