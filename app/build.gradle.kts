fdf24260b8ca74276906735b87b80e63c9c4db1d
if <1096005725@qq.com>
2026-07-12 13:44:20 +0800

    chore: bump version to v3.2.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.skypulse.weather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 789
-        versionName = "3.2.15"
+        versionCode = 790
+        versionName = "3.2.16"
 
         vectorDrawables {
             useSupportLibrary = true
